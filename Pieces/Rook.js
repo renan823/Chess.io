ef9787edc0e6a3badea2f87f981138c0f66facec
Renan <70537760+renan823@users.noreply.github.com>
2022-11-26 13:08:23 -0300

    Rook moves update (complete)

--- Pieces/Rook.js
+++ Pieces/Rook.js
@@ -1,46 +1,80 @@
 class Rook extends Piece{
-    constructor(c, r, color, moveStepX, moveStepY){
-        super(c, r, color, moveStepX, moveStepY, "rook", 0);
+    constructor(c, r, color){
+        super(c, r, color, 7, 7, "rook", 0);
     }
 
-    
     movePossibilities(board){
         let actualRowIndex = this.r;
         let actualColumnIndex = this.c;
-
         let possibleMoves = []
+
         //up move
-        if(!actualRowIndex == 0){
+        if(actualRowIndex != 0){
             for(let i = actualRowIndex-1; i >= 0; i--){
                 let square = board[i][actualColumnIndex];
                 if(this.hasPiece(square)){
                     if(this.canKill(square)){
-                        possibleMoves.push({r: i, c: this.c-1, kill: true});
+                        possibleMoves.push({r: i, c: actualColumnIndex, kill: true});
                         break;
                     }
                     break;
                 }
                 else{
-                    possibleMoves.push({r: i, c: this.c-1, kill: false});
+                    possibleMoves.push({r: i, c: actualColumnIndex, kill: false});
                 }
             }
         }
 
         //down move
-        if(!this.r-1 === board.length){
-            
+        if(actualRowIndex != board.length){
+            for(let i = actualRowIndex+1; i < board.length; i++){
+                let square = board[i][actualColumnIndex];
+                if(this.hasPiece(square)){
+                    if(this.canKill(square)){
+                        possibleMoves.push({r: i, c: actualColumnIndex, kill: true});
+                        break;
+                    }
+                    break;
+                }
+                else{
+                    possibleMoves.push({r: i, c: actualColumnIndex, kill: false});
+                }
+            }
         }
 
         //left move
-        if(!this.c-1 === 0){
-
+        if(actualColumnIndex != 0){
+            for(let i = actualColumnIndex-1; i >= 0; i--){
+                let square = board[actualRowIndex][i];
+                if(this.hasPiece(square)){
+                    if(this.canKill(square)){
+                        possibleMoves.push({r: actualRowIndex, c: i, kill: true});
+                        break;
+                    }
+                    break;
+                }
+                else{
+                    possibleMoves.push({r: actualRowIndex, c: i, kill: false});
+                }
+            }
         }
 
         //right move
-        if(!this.c-1 === board.length){
-
+        if(actualColumnIndex != board.length){
+            for(let i = actualColumnIndex+1; i < board.length; i++){
+                let square = board[actualRowIndex][i];
+                if(this.hasPiece(square)){
+                    if(this.canKill(square)){
+                        possibleMoves.push({r: actualRowIndex, c: i, kill: true});
+                        break;
+                    }
+                    break;
+                }
+                else{
+                    possibleMoves.push({r: actualRowIndex, c: i, kill: false});
+                }
+            }
         }
-
 
         return possibleMoves;
     }
